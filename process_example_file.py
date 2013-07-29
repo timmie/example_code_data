4a3d030fea83a675631242eed1551351923a0c1e
TimMi <timmichelsen@gmx-topmail.de>
2013-07-29 11:40:14 +0200

    added case 1904 date

--- process_example_file.py
+++ process_example_file.py
@@ -14,7 +14,7 @@ sheet = 'min'
 
 
 ### this fails:
-df_in = pd.read_excel(filepath, sheet, skiprows=12, header=10, index_col=1)
+df_in = pd.read_excel(filepath, sheet, skiprows=11, header=10, index_col=1)
 
 ### workaround
 
@@ -29,3 +29,15 @@ df_corr =xlsall.parse(sheet, skiprows=11, header=10, index_col=1)
 
 df_corr.columns
 
+
+### check dedicated 1904 file
+
+
+
+filename_1904 = 'example_file_2013-07-25_1904-dates.xlsx'
+filepath_1904 = os.path.join(os.getcwd(), filename_1904)
+sheet = 'min'
+
+
+df_in_1904 = pd.read_excel(filepath_1904, sheet, skiprows=11, 
+                      header=10, index_col=1)
